9d281ea6bf11df0b55e1b2881273494a144b043f
رأفت عمر <shamwebs1@gmail.com>
2026-05-03 11:00:34 +0300

    Update alm_vm.js

--- alm_vm.js
+++ alm_vm.js
@@ -1,95 +1,68 @@
-/****************************************************
- * ALM VM v2 (Improved + Consistent)
- ****************************************************/
-
 class ALM_VM {
-  constructor() {
-    this.stack = [];
-    this.vars = {};
+  constructor(){
     this.programs = {};
+    this.vars = {};
   }
 
   reset(){
-    this.stack = [];
     this.vars = {};
   }
 
-  loadProgram(name, instructions){
-    this.programs[name] = instructions;
+  loadProgram(name, ins){
+    this.programs[name]=ins;
   }
 
-  run(name, input){
-    const prog = this.programs[name];
-    if(!prog) throw new Error("Program not found: " + name);
+  run(name,input){
+    const p=this.programs[name];
+    if(!p) throw new Error("missing program");
 
     this.reset();
 
-    this.vars.input = input;
-    this.vars.pos = 0;
-    this.vars.acc = 0n;
-    this.vars.str = "";
-    this.vars.tmp = 0n;
+    this.vars.input=input;
+    this.vars.pos=0;
+    this.vars.acc=0n;
+    this.vars.str="";
+    this.vars.tmp=0n;
 
-    let ip = 0;
+    let ip=0;
 
-    while(ip < prog.length){
-      const ins = prog[ip];
-      const op = ins[0];
+    while(ip<p.length){
+      const op=p[ip][0];
 
       switch(op){
 
         case "RESET_ACC":
-          this.vars.acc = 0n;
-          this.vars.pos = 0;
+          this.vars.acc=0n;
+          this.vars.pos=0;
           break;
 
         case "FOR_EACH_CHAR":{
-          const body = ins[1];
-          const input = this.vars.input;
+          const body=p[ip][1];
+          let c=0;
 
-          let count = 0;
-
-          for(let i=input.length-1; i>=0 && count<LMAX; i--, count++){
-
-            const ch = input[i];
-
-            // تجاهل أي حرف غير مدعوم
-            if(!charToIndex[ch]) continue;
-
-            this.vars.ch = ch;
-
-            for(const inner of body){
-              this.exec(inner);
-            }
+          for(let i=input.length-1;i>=0 && c<12;i--,c++){
+            this.vars.ch=input[i];
+            for(const b of body)this.exec(b);
           }
           break;
         }
 
         case "RESET_STR":
-          this.vars.str = "";
-          this.vars.tmp = BigInt(this.vars.input);
+          this.vars.str="";
+          this.vars.tmp=BigInt(input);
           break;
 
         case "FOR_I":{
-          const count = ins[1];
-          const body = ins[2];
-
-          for(let i=0;i<count;i++){
-            for(const inner of body){
-              this.exec(inner);
-            }
+          const n=p[ip][1];
+          const body=p[ip][2];
+          for(let i=0;i<n;i++){
+            for(const b of body)this.exec(b);
           }
           break;
         }
 
-        case "RETURN_ACC":
-          return this.vars.acc;
-
-        case "RETURN_STR":
-          return this.vars.str;
-
-        default:
-          throw new Error("Unknown op: " + op);
+        case "RETURN_ACC": return this.vars.acc;
+        case "RETURN_STR": return this.vars.str;
       }
 
       ip++;
@@ -97,15 +70,13 @@ class ALM_VM {
   }
 
   exec(ins){
-    const op = ins[0];
+    const B=32n;
 
-    const BASE = BigInt(B);
-
-    switch(op){
+    switch(ins[0]){
 
       case "PUSH_CHAR_INDEX":{
-        const idx = charToIndex[this.vars.ch] || 0;
-        this.vars.acc += BigInt(idx) * (BASE ** BigInt(this.vars.pos));
+        const idx=window.charToIndex[this.vars.ch]||0;
+        this.vars.acc+=BigInt(idx)*(B**BigInt(this.vars.pos));
         break;
       }
 
@@ -114,16 +85,16 @@ class ALM_VM {
         break;
 
       case "MOD_BASE":
-        this.vars.mod = Number(this.vars.tmp % BASE);
+        this.vars.mod=Number(this.vars.tmp%B);
         break;
 
       case "DIV_BASE":
-        this.vars.tmp = this.vars.tmp / BASE;
+        this.vars.tmp=this.vars.tmp/B;
         break;
 
       case "APPEND_CHAR":
         if(this.vars.mod){
-          this.vars.str = indexToChar[this.vars.mod] + this.vars.str;
+          this.vars.str=window.indexToChar[this.vars.mod]+this.vars.str;
         }
         break;
     }
